3d6363b450b4c26f2975c20148bbee114aae4c75
Tam Le <tam.le@socialbakers.com>
2019-09-18 15:41:50 +0200

    create users reducer and connect it to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.withCredentials =true;
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
+
+// axios.get('/Users');
+
+const store = createStore(reducers);
+
+ReactDOM.render(<Provider store = {store}> <App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
